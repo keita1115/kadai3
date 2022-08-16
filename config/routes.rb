8cc59e861108fe2167f1f0e637fa458cd58f38e9
keita1115 <keita.hashimoto1115@gmail.com>
2022-08-16 11:04:57 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'homes#top'
+  get'/home/about' => 'homes#about',as: 'about'
+
+  resources :books,only: [:new,:index,:show,:edit,:create,:update,:destroy]
+  resources :users,only: [:edit, :index, :show,:update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
